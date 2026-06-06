e681a2628a9109679cfb0633ee3d4846ba6c54fc
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-06-06 21:46:30 +0300

    ansbile test

---
+++ ansible.py
@@ -0,0 +1,18 @@
+# what is ansible?Ansible is an open-source automation tool that allows you to automate IT tasks such as configuration management,
+
+
+class Ansible:
+    def __init__(self):
+        self.description = "Ansible is an open-source automation tool that allows you to automate IT tasks such as configuration management, application deployment, and orchestration."
+
+    def get_description(self):
+        return self.description
+
+import requests
+import re 
+
+BASE_URL = "http://localhost:8000"
+EMAIL = ""
+PASSWORD = "TestPassword123"
+
+
